1f6769683f654f72b36ed470d1859bf3ee262808
AlbertoGarciaToro <125936109+AlbertoGarciaToro@users.noreply.github.com>
2023-02-22 16:45:25 -0600

    Add files via upload

---
+++ MarcoJuegoGato.java
@@ -0,0 +1,10 @@
+
+public class MarcoJuegoGato {
+//GarciaToroAlberto
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("- - - - -\n|       |\n|       |\n|       |\n- - - - -");
+
+	}
+
+}
