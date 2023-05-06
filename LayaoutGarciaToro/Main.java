304a18861f1bdbd1e5e1fa49a91d41fd29f5a9dd
AlbertoGarciaToro <125936109+AlbertoGarciaToro@users.noreply.github.com>
2023-05-06 16:51:02 -0600

    Add files via upload

---
+++ LayaoutGarciaToro/Main.java
@@ -0,0 +1,8 @@
+package LayaoutGarciaToro;
+
+public class Main {
+    public static void main(String[] args) {
+        //Ventana ventana1=new Ventana("Layauots");
+        VentanaGrid ventana2=new VentanaGrid("Ventana layaout cuadricula");
+    }
+}
